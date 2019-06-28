37eabf0fdd7e8fe5ec3cab7a2c037ad643a9ed7d
Davy Mitchell <daftspaniel@gmail.com>
2019-06-28 20:25:46 +0100

    Add first E2e tests.

---
+++ cypress/integration/main_spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+describe('Basic Text Entry Test', function() {
+  before(() => {
+    cy.visit('http://localhost:3001/')
+    cy.get('.Editor').type('{selectall}Hello world!')
+  })
+
+  it('Entering text into the editor', function() {
+    cy.get('.Editor').should('have.text', 'Hello world!')
+  })
+})
