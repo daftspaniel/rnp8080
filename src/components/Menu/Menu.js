1ec8b3d0d146f43e92d67f4c5a7ed519c6da76fd
Davy Mitchell <daftspaniel@gmail.com>
2019-09-01 14:52:25 +0100

    Menu tweaks.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,54 +6,53 @@ import BaseComponent from '../BaseComponent'
 import './Menu.css'
 
 class Menu extends BaseComponent {
-  render() {
-    return (
-      <div
-        className="Menu"
-        style={this.theme.getColorStyles()}
-        onMouseLeave={e => this.close(e)}
-      >
-        <div onClick={e => this.open(e)}>{this.props.name}</div>
-        {this.state.open && this.props.items ? (
-          <div
-            className="menuH"
-            onMouseLeave={e => this.close(e)}
-            style={this.theme.getColorStyles()}
-          >
-            {this.props.items.map((item, index) =>
-              this.renderMenuItem(item, index)
-            )}
-          </div>
-        ) : null}
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div
+                className="Menu"
+                style={this.theme.getColorStyles()}
+                onMouseLeave={e => this.close(e)}
+                onMouseEnter={e => this.open(e)}
+            >
+                <div onClick={e => this.open(e)}>{this.props.name}</div>
+                {this.state.open && this.props.items ? (
+                    <div
+                        className="menuH"
+                        onMouseLeave={e => this.close(e)}
+                        style={this.theme.getColorStyles()}
+                    >
+                        {this.props.items.map((item, index) =>
+                            this.renderMenuItem(item, index)
+                        )}
+                    </div>
+                ) : null}
+            </div>
+        )
+    }
 
-  renderMenuItem(item, index) {
-    return (
-      <div key={index}>
-        <MenuItem item={item} close={this.close} />
-        {this.getSeparator(item)}
-      </div>
-    )
-  }
+    renderMenuItem(item, index) {
+        return (
+            <div key={index}>
+                <MenuItem item={item} close={this.close} />
+                {this.getSeparator(item)}
+            </div>
+        )
+    }
 
-  getMenuStyles = () => this.theme.getColorStyles()
+    getMenuStyles = () => this.theme.getColorStyles()
 
-  getSeparator(item) {
-    return item.separator ? (
-      <div className="menuSeparator" style={this.theme.getColorStyles()}>
-        &nbsp;
-      </div>
-    ) : null
-  }
+    getSeparator(item) {
+        return item.separator ? (
+            <div className="menuSeparator" style={this.theme.getColorStyles()}></div>
+        ) : null
+    }
 
-  open = e => this.setState({ open: true })
+    open = e => this.setState({ open: true })
 
-  close = e => {
-    this.setState({ open: false })
-    return false
-  }
+    close = e => {
+        this.setState({ open: false })
+        return false
+    }
 }
 
 export default Menu
