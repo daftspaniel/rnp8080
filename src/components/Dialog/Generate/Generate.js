249f5ab4ea5c9459ee12e3dd7099ccc39345b69f
Davy Mitchell <daftspaniel@gmail.com>
2019-09-17 13:36:09 +0100

    Populate generate dialog. Lint String process.

--- src/components/Dialog/Generate/Generate.js
+++ src/components/Dialog/Generate/Generate.js
@@ -8,22 +8,49 @@ import './Generate.css'
 import { Dialog } from '../Dialog'
 
 class GenerateDialog extends Dialog {
-    constructor(props) {
-        super(props)
-        this.minibus.subscribe(EditorEvents.Show_Generate_Dialog, () => this.show())
-        this.title = 'Generate'
-    }
+  constructor(props) {
+    super(props)
+    this.minibus.subscribe(EditorEvents.Show_Generate_Dialog, () => this.show())
+    this.title = 'Generate'
+  }
 
-    render() {
-        if (!this.state.visible) return null
-        return (
-            <div className="dialogPanel GenerateDialog" style={this.theme.getColorStyles()}>
-                {this.renderTitleBar()}
-                <br />
-                <button onClick={this.close}>Close</button>
-            </div >
-        )
-    }
+  render() {
+    if (!this.state.visible) return null
+
+    return this.renderDragbar(() => (
+      <div
+        className="dialogPanel GenerateDialog"
+        style={this.theme.getColorStyles()}
+      >
+        {this.renderTitleBar()}
+        <br />
+        <label class="repeatLabel">Repeat</label>
+        <input
+          class="repeat"
+          type="text"
+          placeholder="Type text here..."
+          id="repeatTextbox"
+        />
+        <input class="repeatCount" type="number" min="1" /> Times
+        <br />
+        <br />
+        <input type="checkbox" /> Add a newline after each item
+        <br />
+        <br />
+        <textarea
+          class="previewText"
+          readonly
+          placeholder="Preview will appear here"
+        ></textarea>
+        <br />
+        <button class="actionButton">Prepend</button>
+        <button class="actionButton">Insert</button>
+        <button class="actionButton">Append</button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <button onClick={this.close}>Close</button>
+      </div>
+    ))
+  }
 }
 
 export default GenerateDialog
--- src/lib/StringProcess.test.js
+++ src/lib/StringProcess.test.js
@@ -44,26 +44,26 @@ it('Counts sentences correctly.', () => {
 })
 
 it('Prefix each line correctly.', () => {
-  expect(tp.prefixLines("", "TEST")).toEqual("TEST")
-  expect(tp.prefixLines("a\nb\n", "TEST")).toEqual("TESTa\nTESTb\nTEST")
-  expect(tp.prefixLines("asdf\nxyzz\n", "12345")).toEqual("12345asdf\n12345xyzz\n12345")
-  expect(tp.prefixLines("Coffee", "Cup")).toEqual("CupCoffee")
+  expect(tp.prefixLines('', 'TEST')).toEqual('TEST')
+  expect(tp.prefixLines('a\nb\n', 'TEST')).toEqual('TESTa\nTESTb\nTEST')
+  expect(tp.prefixLines('asdf\nxyzz\n', '12345')).toEqual('12345asdf\n12345xyzz\n12345')
+  expect(tp.prefixLines('Coffee', 'Cup')).toEqual('CupCoffee')
 })
 
 it('Postfix each line correctly.', () => {
-  expect(tp.postfixLines("", "TEST")).toEqual("TEST")
-  expect(tp.postfixLines("a\nb\n", "TEST")).toEqual("aTEST\nbTEST\nTEST")
-  expect(tp.postfixLines("asdf\nxyzz\n", "12345")).toEqual("asdf12345\nxyzz12345\n12345")
-  expect(tp.postfixLines("Coffee", "Cup")).toEqual("CoffeeCup")
+  expect(tp.postfixLines('', 'TEST')).toEqual('TEST')
+  expect(tp.postfixLines('a\nb\n', 'TEST')).toEqual('aTEST\nbTEST\nTEST')
+  expect(tp.postfixLines('asdf\nxyzz\n', '12345')).toEqual('asdf12345\nxyzz12345\n12345')
+  expect(tp.postfixLines('Coffee', 'Cup')).toEqual('CoffeeCup')
 })
 
 it('Numbers lines correctly.', () => {
-  expect(tp.addNumbering("")).toEqual('')
-  expect(tp.addNumbering("Hello")).toEqual("1. Hello\n")
-  expect(tp.addNumbering("Hello\nWorld\n")).toEqual("1. Hello\n2. World\n")
-  expect(tp.addNumbering("Hello\nWorld\nWorms\n")).toEqual(
-    "1. Hello\n2. World\n3. Worms\n")
-  expect(tp.addNumbering("Hello\n\nWorld\nWorms\n")).toEqual("1. Hello\n\n2. World\n3. Worms\n")
+  expect(tp.addNumbering('')).toEqual('')
+  expect(tp.addNumbering('Hello')).toEqual('1. Hello\n')
+  expect(tp.addNumbering('Hello\nWorld\n')).toEqual('1. Hello\n2. World\n')
+  expect(tp.addNumbering('Hello\nWorld\nWorms\n')).toEqual(
+    '1. Hello\n2. World\n3. Worms\n')
+  expect(tp.addNumbering('Hello\n\nWorld\nWorms\n')).toEqual('1. Hello\n\n2. World\n3. Worms\n')
 })
 
 it('Convert tabs to spaces', () => {
@@ -83,14 +83,14 @@ it('Doublespace text', () => {
 it('Reverse text', () => {
   expect(tp.reverse('')).toEqual('')
   expect(tp.reverse('Moo')).toEqual('ooM')
-  expect(tp.reverse("Zebras are cool!\nMonkeys are okay!\nDogs are the best!\n")).toEqual('Dogs are the best!\nMonkeys are okay!\nZebras are cool!')
+  expect(tp.reverse('Zebras are cool!\nMonkeys are okay!\nDogs are the best!\n')).toEqual('Dogs are the best!\nMonkeys are okay!\nZebras are cool!')
 })
 
 it('Randomise lines', () => {
   expect(tp.randomise('')).toEqual('')
   expect(tp.randomise('Moo')).toEqual('Moo')
-  expect(tp.randomise("Zebras are cool!\nMonkeys are okay!\nDogs are the best!\n").split('\n').length).toEqual(4)
-  expect(tp.randomise("\n\nZebras are cool!\nMonkeys are okay!\nDogs are the best!\n").split('\n').length).toEqual(6)
+  expect(tp.randomise('Zebras are cool!\nMonkeys are okay!\nDogs are the best!\n').split('\n').length).toEqual(4)
+  expect(tp.randomise('\n\nZebras are cool!\nMonkeys are okay!\nDogs are the best!\n').split('\n').length).toEqual(6)
 })
 
 it('Sort alpha - single line', () => {
@@ -98,39 +98,39 @@ it('Sort alpha - single line', () => {
 })
 
 it('Sort alpha - multi line', () => {
-  expect(tp.sort("Zebras are cool!\nMonkeys are okay!\nDogs are the best!")).toEqual("Dogs are the best!\nMonkeys are okay!\nZebras are cool!")
+  expect(tp.sort('Zebras are cool!\nMonkeys are okay!\nDogs are the best!')).toEqual('Dogs are the best!\nMonkeys are okay!\nZebras are cool!')
 })
 
 it('Sort line length', () => {
-  expect(tp.sort("bb\nddddd\na\n")).toEqual("a\nbb\nddddd")
-  expect(tp.sort("bb\nA\n").length).toEqual(4)
+  expect(tp.sort('bb\nddddd\na\n')).toEqual('a\nbb\nddddd')
+  expect(tp.sort('bb\nA\n').length).toEqual(4)
 })
 
 it('Duplicate Lines', () => {
-  expect(tp.duplicateLines("")).toEqual("")
-  expect(tp.duplicateLines("hello")).toEqual("hellohello")
-  expect(tp.duplicateLines("hello\n")).toEqual("hellohello\n")
-  expect(tp.duplicateLines("hello\nthere\nare\napples\nin\nhere.")).toEqual("hellohello\ntherethere\nareare\napplesapples\ninin\nhere.here.")
+  expect(tp.duplicateLines('')).toEqual('')
+  expect(tp.duplicateLines('hello')).toEqual('hellohello')
+  expect(tp.duplicateLines('hello\n')).toEqual('hellohello\n')
+  expect(tp.duplicateLines('hello\nthere\nare\napples\nin\nhere.')).toEqual('hellohello\ntherethere\nareare\napplesapples\ninin\nhere.here.')
 })
 
 it('Duplicate Current Line', () => {
-  expect(tp.duplicateLine("hello\nworld\n", 3)).toEqual("hello\nhello\nworld\n")
-  expect(tp.duplicateLine('This is and intro\ntrouble\nclosing words', 20)).toEqual("This is and intro\ntrouble\ntrouble\nclosing words")
-  expect(tp.duplicateLine("mouse\n", 0)).toEqual("mouse\nmouse\n")
-  expect(tp.duplicateLine("mouse\n", 3)).toEqual("mouse\nmouse\n")
-  expect(tp.duplicateLine("mouse\n", 4)).toEqual("mouse\nmouse\n")
-  expect(tp.duplicateLine("mouse\n", 5)).toEqual("mouse\nmouse\n")
-  expect(tp.duplicateLine("\nmouse", 0)).toEqual("\nmouse")
+  expect(tp.duplicateLine('hello\nworld\n', 3)).toEqual('hello\nhello\nworld\n')
+  expect(tp.duplicateLine('This is and intro\ntrouble\nclosing words', 20)).toEqual('This is and intro\ntrouble\ntrouble\nclosing words')
+  expect(tp.duplicateLine('mouse\n', 0)).toEqual('mouse\nmouse\n')
+  expect(tp.duplicateLine('mouse\n', 3)).toEqual('mouse\nmouse\n')
+  expect(tp.duplicateLine('mouse\n', 4)).toEqual('mouse\nmouse\n')
+  expect(tp.duplicateLine('mouse\n', 5)).toEqual('mouse\nmouse\n')
+  expect(tp.duplicateLine('\nmouse', 0)).toEqual('\nmouse')
 })
 
 it('Generate repeated string', () => {
-  expect(tp.generateRepeatedString("Moo")).toEqual("Moo")
-  expect(tp.generateRepeatedString("Moo",0)).toEqual("")
-  expect(tp.generateRepeatedString("Moo", 4)).toEqual("MooMooMooMoo")
+  expect(tp.generateRepeatedString('Moo')).toEqual('Moo')
+  expect(tp.generateRepeatedString('Moo',0)).toEqual('')
+  expect(tp.generateRepeatedString('Moo', 4)).toEqual('MooMooMooMoo')
 })
 
 it('Generate repeated string with newline', () => {
-  expect(tp.generateRepeatedString("Moo", 1, true)).toEqual("Moo\n")
-  expect(tp.generateRepeatedString("Moo",0,true)).toEqual("")
-  expect(tp.generateRepeatedString("Moo", 4,true)).toEqual("Moo\nMoo\nMoo\nMoo\n")
+  expect(tp.generateRepeatedString('Moo', 1, true)).toEqual('Moo\n')
+  expect(tp.generateRepeatedString('Moo',0,true)).toEqual('')
+  expect(tp.generateRepeatedString('Moo', 4,true)).toEqual('Moo\nMoo\nMoo\nMoo\n')
 })
